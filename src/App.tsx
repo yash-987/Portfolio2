35d19c0aaa92c6b99cf86ddd92a53889de4b57f5
yash987 <ys2599518@gmail.com>
2024-02-29 19:08:08 +0530

    Last changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Home from './Pages/Home/Home';
+
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
